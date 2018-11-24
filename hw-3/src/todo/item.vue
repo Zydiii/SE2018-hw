29cea10bbd711b83cf7a856ecde79c8fa5c80edc
Zydiii <15826138027@163.com>
2018-11-24 21:52:57 +0800

    v2

--- hw-3/src/todo/item.vue
+++ hw-3/src/todo/item.vue
@@ -2,7 +2,7 @@
   <div :class="['todo-item', todo.completed ? 'completed':'']">
     <input 
       type="checkbox"
-      class="taggle"
+      class="toggle"
       v-model="todo.completed"
     >
     <label>{{todo.content}}</label>
@@ -31,8 +31,7 @@ export default {
         position relative
         background #ffffff
         font-size 24px
-        border-bottom 1px solid rgba(0,0,0,.06)
-    // ´Ë´¦ÐÞ¸Ä &    
+        border-bottom 1px solid rgba(0,0,0,.06) 
     .todo-item:hover
         .destroy:after {
             content: 'x'
@@ -50,45 +49,43 @@ export default {
         color #d9d9d9
         text-decoration line-through
     }
-    .toggle{
-        text-align center
-        width 400px
-        height 40px
-        position absolute
-        top 0
-        bottom 0
-        margin auto 0
-        border none
+     .toggle{
+        text-align: center;
+        width: 40px;
+        height: 40px;
+        line-height: 40px
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        margin: auto 0;
+        border: none;
+        appearance: none;
         outline none
-        appearance none
-    }
-    .toggle:before{
-        content:url('../assets/images/bg.svg')
-        position absolute
-        left 12px
+        padding-left 5px
         cursor pointer
+        &:after{
+            content: url('../assets/images/round.svg')
+        }
+        &:checked:after{
+            content: url('../assets/images/done.svg')
+        }
     }
-    .toggle:checked:before{
-        content : url('../assets/images/bg.svg')
-        position absolute
-        left 12px
-        cursor pointer
-    }
-    .destroy
-        position absolute
-        top 50%
-        right 10px
-        bottom 0;
-        width 40px
-        height 40px
-        margin auto 0;
-        font-size 30px
-        color #cc9a9a;
-        margin-bottom 11px
-        transition: color 0.2s east-out
+    .destroy{
+        position: absolute;
+        top: 0;
+        right: 10px;
+        bottom: 0;
+        width: 40px;
+        height: 40px;
+        margin: auto 0;
+        font-size: 30px;
+        color: #cc9a9a;
+        margin-bottom: 11px;
+        transition: color 0.2s ease-out;
         background-color transparent
         appearance none
         border-width 0
         cursor pointer
         outline none
+    }
 </style>
